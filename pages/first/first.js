5c1b9a6cd6965157c09d2137f115e0b3c57cabc7
pluto <1574647887@qq.com>
2023-07-04 22:30:06 +0800

    更改图片显示方式

--- pages/first/first.js
+++ pages/first/first.js
@@ -1,18 +1,7 @@
 // pages/first/first.js
 Page({
-  takePhoto() {
-    const ctx = wx.createCameraContext()
-    ctx.takePhoto({
-      quality: 'high',
-      success: (res) => {
-        this.setData({
-          src: res.tempImagePath
-        })
-      }
-    })
-  },
-  error(e) {
-    console.log(e.detail)
+  takePhoto(e) {
+    console.log('1')
   },
 
   /**
