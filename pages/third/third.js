d548da37d15514c97d20a354bd5ba65784504d5c
pluto <1574647887@qq.com>
2023-07-08 21:04:19 +0800

    备份

--- pages/third/third.js
+++ pages/third/third.js
@@ -1,5 +1,4 @@
 import * as echarts from '../../ec-canvas/echarts';
-
 let chart = null;
 
 Page({
@@ -17,84 +16,87 @@ function initChart(canvas, width, height, dpr) {
     height: height,
     devicePixelRatio: dpr
   })
-
-  canvas.setChart(chart)
-  // 显示Echarts图表类型信息，可以去Echarts官网复制粘贴
-  const person = require('../../utils/images/Veins_Medical_Diagram_clip_art.svg');
-  echarts.registerMap('organ_diagram', { svg: person });
-  let option = {
-    tooltip: {},
-    geo: {
-      left: 10,
-      right: '50%',
-      map: 'organ_diagram',
-      selectedMode: 'multiple',
-      emphasis: {
-        focus: 'self',
-        itemStyle: {
-          color: null
+  wx.request({
+    url: '/utils/images/Veins_Medical_Diagram_clip_art.svg',
+    success:function(res){
+      var svg = res.data
+      echarts.registerMap('organ_diagram', { svg: svg });
+    }
+  })
+      const option = {
+        tooltip: {},
+        geo: {
+          left: 10,
+          right: '50%',
+          map: 'organ_diagram',
+          selectedMode: 'multiple',
+          emphasis: {
+            focus: 'self',
+            itemStyle: {
+              color: null
+            },
+            label: {
+              position: 'bottom',
+              distance: 0,
+              textBorderColor: '#fff',
+              textBorderWidth: 2
+            }
+          },
+          blur: {},
+          select: {
+            itemStyle: {
+              color: '#b50205'
+            },
+            label: {
+              show: false,
+              textBorderColor: '#fff',
+              textBorderWidth: 2
+            }
+          }
         },
-        label: {
-          position: 'bottom',
-          distance: 0,
-          textBorderColor: '#fff',
-          textBorderWidth: 2
-        }
-      },
-      blur: {},
-      select: {
-        itemStyle: {
-          color: '#b50205'
+        grid: {
+          left: '60%',
+          top: '20%',
+          bottom: '20%'
         },
-        label: {
-          show: false,
-          textBorderColor: '#fff',
-          textBorderWidth: 2
-        }
-      }
-    },
-    grid: {
-      left: '60%',
-      top: '20%',
-      bottom: '20%'
-    },
-    xAxis: {},
-    yAxis: {
-      data: [
-        'heart',
-        'large-intestine',
-        'small-intestine',
-        'spleen',
-        'kidney',
-        'lung',
-        'liver'
-      ]
-    },
-    series: [
-      {
-        type: 'bar',
-        emphasis: {
-          focus: 'self'
+        xAxis: {},
+        yAxis: {
+          data: [
+            'heart',
+            'large-intestine',
+            'small-intestine',
+            'spleen',
+            'kidney',
+            'lung',
+            'liver'
+          ]
         },
-        data: [121, 321, 141, 52, 198, 289, 139]
-      }
-    ]
-  };
-  chart.setOption(option);
-  chart.on('mouseover', { seriesIndex: 0 }, function (event) {
-    chart.dispatchAction({
-      type: 'highlight',
-      geoIndex: 0,
-      name: event.name
-    });
-  });
-  chart.on('mouseout', { seriesIndex: 0 }, function (event) {
-    chart.dispatchAction({
-      type: 'downplay',
-      geoIndex: 0,
-      name: event.name
-    });
-  });
+        series: [
+          {
+            type: 'bar',
+            emphasis: {
+              focus: 'self'
+            },
+            data: [121, 321, 141, 52, 198, 289, 139]
+          }
+        ]
+      };
+      canvas.setChart(chart)
+      chart.setOption(option)
+      chart.on('mouseover', { seriesIndex: 0 }, function (event) {
+        chart.dispatchAction({
+          type: 'highlight',
+          geoIndex: 0,
+          name: event.name
+        });
+      });
+      chart.on('mouseout', { seriesIndex: 0 }, function (event) {
+        chart.dispatchAction({
+          type: 'downplay',
+          geoIndex: 0,
+          name: event.name
+        });
+      });
   return chart;
 }
 
